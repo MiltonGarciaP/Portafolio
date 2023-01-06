c40d511c9091b9c0f4610ecce8ab39b04cfbe3d0
MiltonDuro <87989658+MiltonDuro@users.noreply.github.com>
2023-01-06 10:34:28 -0400

    Abou END

---
+++ src/components/About.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            Hola , Soy Milton Garcia
+            <br className="hidden lg:inline-block" />Desarrollador de Software Backend
+          </h1>
+          <p className="mb-7 leading-relaxed">
+            Soy un desarrollador de Software de 20 años que viven Santo Domingo, Republica Dominicana
+            soy tecnologo en Desarrollo de Software en el Instituto Tecnologico de las Americas (ITLA)
+            tengo  experiencia trabajando con diferentes tecnologias pero con la cual me siento comodo es
+            con la biblioteca de .net de microsoft con el lenguaje de programacion C# (ASP.NET , Windows Forms , Xamarin , Blazor y MAUI)
+          </p>
+          <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+              Contactame
+            </a>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+              Mis Proyectos
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="/icono.jpg"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
